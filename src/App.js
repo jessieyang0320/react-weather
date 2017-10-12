cba109b4a061f5a94eb56e83ab1e3442427122b4
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-10-12 19:49:36 -0400

    create a searchbar component and connect it with redux, use axios to make request

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+// import './App.css';
+import SearchBar from './containers/search_bar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         weather report
+        <SearchBar/>
       </div>
     );
   }
