3af377a43ca2a5d8e7956ec482aae69e5fe116bd
Jessie Yang <jessieyang@Jessies-MacBook-Air.local>
2017-10-12 21:54:22 -0400

    use react sparkline to show charts, error occurs

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Sparklines  from 'react-sparklines';
+
+
 
 class WeatherList extends Component {
 
 	renderWeather(cityData){
-		const name = cityData.city.name
+		const name = cityData.city.name;
+		const temps = cityData.list.map(weather => weather.main.temp)
+		const pressures = cityData.list.map(weather => weather.main.pressure)
+		const humidities = cityData.list.map(weather => weather.main.humidity)
 
 		return (
 			<tr key={name}>
-				<td>{name}</td>
+				<td>
+					{name}
+				</td>
+				
+
 			</tr>
 			)
 
